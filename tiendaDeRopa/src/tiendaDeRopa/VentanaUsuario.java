9d4e2a1fc96c7a4f49a75532f3100040a0ec5551
Iñigo Picon <inigo.picon@opendeusto.es>
2021-11-08 17:57:59 +0100

    Cambio de tipo el atributo permisos

--- tiendaDeRopa/src/tiendaDeRopa/VentanaUsuario.java
+++ tiendaDeRopa/src/tiendaDeRopa/VentanaUsuario.java
@@ -26,7 +26,7 @@ public class VentanaUsuario extends JFrame {
 
 	public VentanaUsuario() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(345, 700, 345, 700);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
