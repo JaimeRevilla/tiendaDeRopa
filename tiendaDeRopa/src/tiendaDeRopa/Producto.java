4d0a906d64bd5bde58c558cd2361a13c7f443250
jonmm <jonmm@DESKTOP-B9PEJRJ>
2021-11-08 12:55:42 +0100

    clasesdd

---
+++ tiendaDeRopa/src/tiendaDeRopa/Producto.java
@@ -0,0 +1,18 @@
+package tiendaDeRopa;
+
+import java.io.Serializable;
+
+public class Producto implements Serializable{
+	
+	private String codigo;
+	private String nombre;
+	private double precio;
+	private int stock;
+	private String descripcion;
+	private String detalles;
+	
+
+	
+	
+	
+}
