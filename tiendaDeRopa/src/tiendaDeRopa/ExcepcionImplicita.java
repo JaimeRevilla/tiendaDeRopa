4717a296783825437cada745091362a94c28a360
Iñigo Picon <inigo.picon@opendeusto.es>
2021-11-08 17:33:23 +0100

    Clase ExcepcionImplicita

--- tiendaDeRopa/src/tiendaDeRopa/ExcepcionImplicita.java
+++ tiendaDeRopa/src/tiendaDeRopa/ExcepcionImplicita.java
@@ -1,5 +1,22 @@
 package tiendaDeRopa;
 
 public class ExcepcionImplicita {
-	private int hola;
+	
+	private String mensaje;
+	
+	public ExcepcionImplicita(String m) {
+		mensaje = m;
+	}
+	
+	
+	/**
+	 * METODO QUE SIRVE PARA PODER MOSTRAR EL MENSAJE QUE LE PASEMOS POR PARAMETRO
+	 */
+	public String toString() {
+		return mensaje;
+	}
+	
+	
+	
+	
 }
