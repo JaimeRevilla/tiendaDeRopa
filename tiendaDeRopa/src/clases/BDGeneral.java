03c02065a627683df3491064759981f5fa28362d
Jaimerevilla <jaimerevilla@opendeusto.es>
2021-11-11 13:32:30 +0100

    Cambios Bd

--- tiendaDeRopa/src/clases/BDGeneral.java
+++ tiendaDeRopa/src/clases/BDGeneral.java
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class BDUsuario {
+public class BDGeneral {
 
 	
 	String Hola;
@@ -191,3 +191,4 @@ public class BDUsuario {
 	}
 	
 }
+
