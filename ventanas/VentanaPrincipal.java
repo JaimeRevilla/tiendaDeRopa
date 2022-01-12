42ae5e7bfbdcaafda4e4e0f3ca475db17931a10d
Jon <jonpar@opendeusto.es>
2022-01-13 00:20:39 +0100

    Los usuarios Jon y Joonpaar es el mismo, pero no sabia que habia que poner el email a la hora de hacer los commits.

--- ventanas/VentanaPrincipal.java
+++ ventanas/VentanaPrincipal.java
@@ -117,6 +117,7 @@ public class VentanaPrincipal extends JFrame {
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
+		System.out.println("Hola");
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
